928704f3a4b437db35824580c96db62289dc9534
Sadam Altoubasi <sadam.altoubasi@yahoo.com>
2025-12-14 17:07:14 +0300

    new EKS version 1.33

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -4,14 +4,14 @@ module "eks" {
 
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.33"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+    ami_type = "BOTTLEROCKET_x86_64"
   }
 
   eks_managed_node_groups = {
