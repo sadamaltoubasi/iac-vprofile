c765501d08047924caf4d0790e51ceea511d8bf4
Sadam Altoubasi <sadam.altoubasi@yahoo.com>
2025-12-14 13:12:23 +0300

    Format terraform files

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.19.1"
 
-  cluster_name    = local.cluster_name
+  cluster_name = local.cluster_name
 
   cluster_version = "1.33"
 
