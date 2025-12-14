7be4292a7c537e8069416d6622013cc71568e0a9
Sadam Altoubasi <sadam.altoubasi@yahoo.com>
2025-12-14 16:37:51 +0300

    ami new update

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -10,8 +10,8 @@ module "eks" {
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
-  ami_type = "AL2023_x86_64_STANDARD"
-}
+    ami_type = "AL2023_x86_64_STANDARD"
+  }
 
   eks_managed_node_groups = {
     one = {
