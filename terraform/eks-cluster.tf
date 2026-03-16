43cbf5eaed4316fe449557678f7140d8c80f791a
Sadam Altoubasi <sadam.altoubasi@yahoo.com>
2026-03-16 14:38:30 +0300

    new edit

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.19.1"
+  version = "~> 20.0"
 
   cluster_name    = local.cluster_name
   cluster_version = "1.29"
@@ -10,7 +10,7 @@ module "eks" {
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+    ami_type = "AL2023_x86_64_STANDARD"
 
   }
 
