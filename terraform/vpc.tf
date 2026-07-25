9a58e7efac09c17d4ed92f292be9a71b9df785d7
Sadam Altoubasi <sadam.altoubasi@yahoo.com>
2026-07-25 13:33:35 +0300

    test

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,9 +10,12 @@ module "vpc" {
   private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
   public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
+  enable_nat_gateway      = true
+  one_nat_gateway_per_az  = true
+  #single_nat_gateway      = true
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
+  map_public_ip_on_launch = true
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
